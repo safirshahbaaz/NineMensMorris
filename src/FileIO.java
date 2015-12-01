9bfe2f7b5f6c66a9cd2c6ce39a96a48ec2f6610d
adykumar <swadhyayak@gmail.com>
2015-11-30 19:31:03 -0800

    Swad wrote code for FileIOgit log -1

--- src/FileIO.java
+++ src/FileIO.java
@@ -3,9 +3,94 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.DataInputStream;
 
 /* Utility Class for File I/O */
 public class FileIO {
+	
+	/*###Function to receive input and place piece on the board during game first phase*/
+	public static boolean placeNewPiece(NineMensMorris obj) throws IOException{
+		while (true){
+			try{
+			
+				System.out.println("\nWhere do you want to place WHITE piece?:");
+				DataInputStream dis = new DataInputStream(System.in);
+				int n = dis.readInt();
+				PositionValue p= obj.getValueAtPosition(n);
+				if (p == PositionValue.X) {
+					obj.setPositionValue(PositionValue.W, n);
+					if (NineMensMorrisLogic.isCloseMill(n, obj)) removePiece(obj);
+					return true;
+				}
+				else{
+					System.out.println("\nThere's already a piece there genius!");
+				}
+			}
+			catch(Exception e){
+				System.out.println("\nError while accepting Input...");
+			}
+		}
+	}
+	
+	/*###Function to receive input and place piece on board: during mid game*/
+	public static boolean movePiece(NineMensMorris obj) throws IOException{
+		while (true){
+			try{
+				System.out.println("\nWhich WHITE piece do you want to move?:");
+				DataInputStream dis = new DataInputStream(System.in);
+				int from = dis.readInt();
+				PositionValue p= obj.getValueAtPosition(from);
+				if (p != PositionValue.W) {
+					System.out.println("\nYou do realize there is no WHITE piece there?");
+					continue;
+				}
+				while (true){
+					System.out.println("Where do you want to place your WHITE piece?:");
+					DataInputStream diss = new DataInputStream(System.in);
+					int to = diss.readInt();
+					PositionValue p1= obj.getValueAtPosition(to); 
+					if (p1 == PositionValue.X) {
+							obj.setPositionValue(PositionValue.W,to);
+							obj.setPositionValue(PositionValue.X,from);
+							System.out.println("\nWHITE moved...");
+							if (NineMensMorrisLogic.isCloseMill(to, obj)) removePiece(obj);
+							return true;
+					}
+					else {
+							System.out.println("\nNope, Can't move there buddy!");
+							continue;
+					}
+				}
+			}
+			catch(Exception e){
+				System.out.println("Error while accepting Input!");
+			}
+		}
+	}
+	
+	
+	/*### Function to remove opponent's piece once your mill is formed...take note that you cannot remove pieces that are part of a mill*/
+	public static boolean removePiece(NineMensMorris obj) throws IOException{
+		while (true){
+			try{
+				System.out.println("\nWhich BLACK piece do you want to remove?:");
+				DataInputStream dis = new DataInputStream(System.in);
+				int n = dis.readInt();
+				PositionValue p= obj.getValueAtPosition(n);
+				if (p == PositionValue.B && !NineMensMorrisLogic.isCloseMill(n, obj)){
+					obj.setPositionValue(PositionValue.X,n);	
+					return true;
+				}
+				else System.out.println("\nInvalid Position, Can't remove that buddy!");
+				continue;
+			}
+			catch(Exception e){
+				System.out.println("Error while accepting Input");
+			}
+		}
+	}
+	
+
 	
 	/* Read from the file and initialize the board */
 	public static NineMensMorris getInputPositions(String path) throws IOException {
